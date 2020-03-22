6078f48046680f3501b9eeee80e0bc32432b0ba5
ailequal <37016865+ailequal@users.noreply.github.com>
2020-03-22 19:17:49 +0100

    Accessing database student info through dd() inside the controller

--- app/Http/Controllers/DatabaseController.php
+++ app/Http/Controllers/DatabaseController.php
@@ -1,15 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Student;
 use Illuminate\Http\Request;
 
 class DatabaseController extends Controller
 {
-		// private $data;
+		private $data;
+		private $studentOne;
+		private $studentTwo;
+
 		public function __construct() {
-			$this->data = App\Student::all();
+			$this->data = Student::all();
+			dd($this->data);
 		}
+
 		public function students() {
 			return view('webpages.students', $this->data);
 		}
